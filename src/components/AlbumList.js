393a8c1a57d3803adbf1c7fb7406869a24cfa352
Angelo Amadora <amadora.angelo@gmail.com>
2018-04-06 10:53:38 +0800

    Revert "made sections"
    This reverts commit e80fc62ec2f5da0450f874fe839528ec23e98a56.

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,7 +1,6 @@
 import React, { Component }  from 'react';
 import { Text, View } from 'react-native';
 import axios from 'axios';
-import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
 
@@ -14,7 +13,7 @@ class AlbumList extends Component {
 
     renderAlbums(){
         return this.state.albums.map( album => 
-            <AlbumDetail key={ album.title } albumProp={album} />
+            <Text key={ album.title }>{album.title}</Text> 
         );
     }
 
