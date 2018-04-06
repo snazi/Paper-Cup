606da6740f3fd18fa2f5f3f9780511682c99556e
Angelo Amadora <amadora.angelo@gmail.com>
2018-04-06 11:17:11 +0800

    headerTextStyleImplementation

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -5,19 +5,24 @@ import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
   const { title, artist, thumbnail_image } = album;
-  const { thumbnailStyle, headerContentStyle } = styles;
+  const {
+        thumbnailStyle,
+        headerContentStyle,
+        thumbnailContainerStyle,
+        headerTextStyle
+    } = styles;
 
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <Image
             style={thumbnailStyle}
             source={{ uri: thumbnail_image }}
           />
         </View>
         <View style={headerContentStyle}>
-          <Text>{title}</Text>
+          <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
       </CardSection>
@@ -33,6 +38,15 @@ const styles = {
   thumbnailStyle: {
     height: 50,
     width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  headerTextStyle: {
+    fontSize: 18
   }
 };
 
